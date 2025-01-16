ed470387ba97f6a54fefddc0b1dcc628369fa0c0
Aleksandr <metelskiy.al@mail.ru>
2025-01-16 22:34:25 +0300

    Added logic of switching colors

--- RedYellowGreen/ViewController.swift
+++ RedYellowGreen/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentSquareIndex = 0
 
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var reedLightView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        reedLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
+        greenLightView.alpha = 0.3
+        
+        reedLightView.layer.cornerRadius = 50
+        yellowLightView.layer.cornerRadius = 50
+        greenLightView.layer.cornerRadius = 50
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeLightDidTup(_ sender: UIButton) {
+        reedLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
+        greenLightView.alpha = 0.3
+        
+        sender.setTitle("NEXT", for: .normal)
+        
+        switch currentSquareIndex {
+        case 0:
+            reedLightView.alpha = 1.0
+        case 1:
+            yellowLightView.alpha = 1.0
+        case 2:
+            greenLightView.alpha = 1.0
+        default:
+            break
+        }
+        
+        currentSquareIndex = (currentSquareIndex + 1) % 3
+    }
+    
 }
 
